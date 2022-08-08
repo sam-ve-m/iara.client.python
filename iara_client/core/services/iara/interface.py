a3d7b19ad8654cf9957b0e34655908a818bb2b67
Vinicius Reis <vinicius.reis@lionx.com.br>
2022-08-08 14:20:25 -0300

    change variable, methods name.

--- iara_client/core/services/iara/interface.py
+++ iara_client/core/services/iara/interface.py
@@ -7,7 +7,7 @@ from iara_client.domain.enums.status import IaraClientStatus
 class IIaraService(ABC):
     @classmethod
     @abstractmethod
-    async def send_to_persephone(
+    async def send_to_iara(
         cls, topic: str, partition: int, message: dict, schema_name: str
     ) -> Tuple[bool, IaraClientStatus]:
         pass
