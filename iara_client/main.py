cc7144e2f0cd7352b234890e2f856ecb629f4b1c
Marco Sievers de Almeida <msa@lionx.com.br>
2022-08-23 10:51:41 -0300

    fix: add dw_account

--- iara_client/main.py
+++ iara_client/main.py
@@ -17,8 +17,9 @@ class Iara:
         :param message: dict
         :return: Returns [process_status: bool, iara_status: IaraClientStatus]
         """
-        (is_message_sent, iara_client_status,) = await IaraService.send_to_iara(
-            topic=topic, message=message
-        )
+        (
+            is_message_sent,
+            iara_client_status,
+        ) = await IaraService.send_to_iara(topic=topic, message=message)
 
         return is_message_sent, iara_client_status
