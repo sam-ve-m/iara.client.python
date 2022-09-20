9bc25979db9b06c4759924ac0ab846bfd9e9bd12
Samuel Veloso Marçal <svm@lionx.com.br>
2022-09-20 16:25:29 -0300

    feat: replaced specific schemas by base schema

--- iara_client/domain/enums/schema/options/enum.py
+++ iara_client/domain/enums/schema/options/enum.py
@@ -1,16 +1,13 @@
 from enum import Enum
 
 from iara_client.domain.schemas.base import BaseSchema
-from iara_client.domain.schemas.caf.schema import CafCpfValidationDetailsRequirements, CafCpfValidationRequirements, \
-    CafScoreValidationDetailsRequirements, CafScoreValidationRequirements
-from iara_client.domain.schemas.ouroinvest.schema import OuroInvestRequirements
 
 
 class SchemaOptions(Enum):
-    CAF_CPF_VALIDATION = CafCpfValidationRequirements
-    CAF_CPF_VALIDATION_DETAILS = CafCpfValidationDetailsRequirements
-    CAF_SELFIE_VALIDATION = CafScoreValidationRequirements
-    CAF_SELFIE_VALIDATION_DETAILS = CafScoreValidationDetailsRequirements
+    CAF_CPF_VALIDATION = BaseSchema
+    CAF_CPF_VALIDATION_DETAILS = BaseSchema
+    CAF_SELFIE_VALIDATION = BaseSchema
+    CAF_SELFIE_VALIDATION_DETAILS = BaseSchema
     CAF_DOCUMENT_VALIDATION = BaseSchema
     SINACOR_REGISTRATION = BaseSchema
     SINACOR_UPDATE = BaseSchema
@@ -20,7 +17,7 @@ class SchemaOptions(Enum):
     DW_ACCOUNT = BaseSchema
     DW_DOCUMENT_VALIDATION = BaseSchema
     OURO_INVESTE_BASIC_REGISTRATION = BaseSchema
-    OURO_INVESTE_BASIC_REGISTRATION_DETAILS = OuroInvestRequirements
+    OURO_INVESTE_BASIC_REGISTRATION_DETAILS = BaseSchema
     OURO_INVESTE_UPGRADE_REGISTRATION = BaseSchema
     BANKPRO_REGISTRATION = BaseSchema
     EMAIL_VALIDATION = BaseSchema
