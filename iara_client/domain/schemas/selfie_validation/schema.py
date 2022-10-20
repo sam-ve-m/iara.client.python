e9ddd2e3cf04edaa9ec6050e44c40b605a6a1d17
Samuel V M <samuel.marcal@ligainvest.com.br>
2022-10-20 09:32:02 -0300

    feat: added CAF requirements for M2 in selfie schema

--- iara_client/domain/schemas/selfie_validation/schema.py
+++ iara_client/domain/schemas/selfie_validation/schema.py
@@ -1,5 +1,31 @@
+from pydantic import BaseModel
 from iara_client.domain.schemas.base import BaseSchema
 
 
+class DeviceInformation(BaseModel):
+    dt: int
+    cm_fl: bool
+    mp: int
+    cm_num: int
+    p_count: int
+    acc_n: str
+    acc_v: str
+    bd: str
+    hw: str
+    md: str
+    boa: str
+    cpu: str
+    md_name: str
+    p_mem: int
+    nfc: bool
+    bio: bool
+    iim: str
+
+
 class SelfieValidationSchema(BaseSchema):
-    device_info: str
+    device_info: DeviceInformation
+    device_id: str
+    latitude: int
+    longitude: int
+    precision: int
+    ip_address: str
