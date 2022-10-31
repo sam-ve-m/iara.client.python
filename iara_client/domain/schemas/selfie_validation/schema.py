0591b8d35f6a75ee09359d778d00a7683d8954e9
Samuel V M <samuel.marcal@ligainvest.com.br>
2022-10-31 16:57:37 -0300

    feat: made_device_info_an_ordinary_dict

--- iara_client/domain/schemas/selfie_validation/schema.py
+++ iara_client/domain/schemas/selfie_validation/schema.py
@@ -1,29 +1,8 @@
-from pydantic import BaseModel
 from iara_client.domain.schemas.base import BaseSchema
 
 
-class DeviceInformation(BaseModel):
-    dt: int
-    cm_fl: bool
-    mp: int
-    cm_num: int
-    p_count: int
-    acc_n: str
-    acc_v: str
-    bd: str
-    hw: str
-    md: str
-    boa: str
-    cpu: str
-    md_name: str
-    p_mem: int
-    nfc: bool
-    bio: bool
-    iim: str
-
-
 class SelfieValidationSchema(BaseSchema):
-    device_info: DeviceInformation
+    device_info: dict
     device_id: str
     latitude: float
     longitude: float
